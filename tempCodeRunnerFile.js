03fb7d4d39e4217687a8fc512ac37128d918489f
JunayedHassan <junayed.adarsha@gmail.com>
2020-07-30 12:24:57 +0600

    aded files

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+erateNumber = document.getElementById('generate-number');
+// document.getElementById('generate-pin').addEventListener('click', function(){
+//     const randomNumber = Math.floor(Math.random() *(9999 - 1000 + 1)) + 1000; // need to check it later
+//     generateNumber.value = randomNumber;
+//     lessThan.style.display = "none";
+// });
